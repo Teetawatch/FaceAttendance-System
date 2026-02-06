ea5a1d10891f1d637ade82a0a23cf7a48ce0d3d1
Teetawatch <teetawatch.p@gmail.com>
2026-02-06 14:28:38 +0700

    fix: use usePublicPath in bootstrap/app.php for more robust shared hosting support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+$app->usePublicPath(base_path('../public_html/faceattendance'));
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
